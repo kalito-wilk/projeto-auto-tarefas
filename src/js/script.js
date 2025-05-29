17945ee1e330a88ae339b26fff28b2de741e7ea4
Kalito Wilk <kalitovieiradasilva11@outlook.com>
2025-05-28 22:57:15 -0300

    modificacoes e atualizacoes de andamento

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,14 @@
-const botao = document.getElementsByTagName('button');
+const horimetro1 = document.getElementById('horimetro-1');
+const horimetro2 = document.getElementById('horimetro-2');
+const horas = document.getElementById('horas');
 
-    console.log(botao);
+function horasCalculadas() {
+
+    const valor1 = parseFloat(horimetro1.value) || 0;
+    
+    const valor2 = parseFloat(horimetro2.value) || 0;
+
+    horas.value = valor2 - valor1;
+}
+
+horimetro2.addEventListener('input', horasCalculadas);
