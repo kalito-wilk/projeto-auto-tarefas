c1a6e6dffa320856d85009770a01cf69c13e277a
Kalito Wilk <kalitovieiradasilva11@outlook.com>
2025-05-29 21:46:22 -0300

    modificacoes e atualizacoes de andamento 06

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,23 @@
+const area = document.getElementById('area');
+
+area.addEventListener('input', () => {
+
+    area.value = area.value.replace(/\s+/g, "").toUpperCase();
+
+});
+
+const percentual = document.getElementById('percentual');
+
+percentual.addEventListener('input', () => {
+
+    let valor = percentual.value.replace('%', ''); // Remove o % se já tiver
+    
+    valor = valor.replace(/\D/g, ''); // Remove qualquer caractere que não seja número
+    
+    percentual.value = valor + '%'; // Adiciona o %
+    
+});
+
 const horimetro1 = document.getElementById('horimetro-1');
 const horimetro2 = document.getElementById('horimetro-2');
 const horas = document.getElementById('horas');
